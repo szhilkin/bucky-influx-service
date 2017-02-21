26d1141107a9562cbb5a56794727ab6eaada925e
Sergey Zhilkin <szhilkin@gmail.com>
2017-02-21 23:03:22 +0300

    Quotes

--- bucky/run.sh
+++ bucky/run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-sed -e "s/{{INFLUXDB_DATABASE}}/$INFLUXDB_DATABASE/"  -e "s/{{INFLUXDB_USERNAME}}/$INFLUXDB_USERNAME/" -e "s/{{INFLUXDB_PASSWORD}}/$INFLUXDB_PASSWORD/" config/default.yaml.template > config/default.yaml
+sed -e "s/{{INFLUXDB_DATABASE}}/$INFLUXDB_DATABASE/" \
+    -e "s/{{INFLUXDB_HOST}}/$INFLUXDB_HOST/" \
+    -e "s/{{INFLUXDB_PORT}}/$INFLUXDB_PORT/" \
+    -e "s/{{BUCKY_PORT}}/$BUCKY_PORT/" \
+    -e "s/{{INFLUXDB_USERNAME}}/$INFLUXDB_USERNAME/" \
+    -e "s/{{INFLUXDB_PASSWORD}}/$INFLUXDB_PASSWORD/" \
+    config/default.yaml.template > config/default.yaml
 
 ./start.js
 
